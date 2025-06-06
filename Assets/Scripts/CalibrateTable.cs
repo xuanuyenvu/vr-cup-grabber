2caebb8d99a41e5d8aa70fc80a1eda6fda1f7e8a
phivu3125 <phivu3125@gmail.com>
2025-06-06 19:10:59 +0700

    pillot 1 2

--- Assets/Scripts/CalibrateTable.cs
+++ Assets/Scripts/CalibrateTable.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using System.IO;
 
 public class CalibrateTable : MonoBehaviour
 {
@@ -10,20 +10,62 @@ public class CalibrateTable : MonoBehaviour
     [SerializeField] private GameObject tableAxis;
     [SerializeField] private GameObject virtualControllerTargetAxis;
 
+    [Header("Calibration Settings")]
+    [SerializeField] private HeadsetCalibrationData calibrationData;
+    [SerializeField] private bool loadPositionOnStart = true;
+    
+    [Header("Auto Save")]
+    [Tooltip("Tự động lưu vị trí khi thoát ứng dụng")]
+    [SerializeField] private bool savePositionOnQuit = true;
+
+    [Header("Debug")]
+    [SerializeField] private bool showDebugInfo = true;
+
+    void Start()
+    {
+        if (loadPositionOnStart && calibrationData != null)
+        {
+            // Tải dữ liệu từ file nếu đang chạy build
+            #if !UNITY_EDITOR
+            LoadCalibrationFromFile();
+            #endif
+            
+            // Áp dụng vị trí đã lưu nếu có
+            if (calibrationData.hasBeenCalibrated)
+            {
+                LoadHeadsetPosition();
+            }
+        }
+    }
+
     void Update()
     {
-        // if (Input.GetKeyDown(KeyCode.Q)) // Nhấn Q để lưu vị trí headset
-        // {
-        //     GetHeadsetPositionRelativeToController();
-        // }
-        // if (Input.GetKeyDown(KeyCode.E)) // Nhấn E để di chuyển controller và điều chỉnh headset
-        // {
-        //     AdjustHeadsetRelativeToTableAxis();
-        // }
         if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.LTouch))
         {
             GetHeadsetPositionRelativeToController();
             AdjustHeadsetRelativeToTableAxis();
+            
+            // Lưu vị trí sau khi hiệu chỉnh
+            SaveHeadsetPosition();
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (savePositionOnQuit && calibrationData != null)
+        {
+            // Lưu vị trí hiện tại
+            calibrationData.SaveCurrentTransform(cameraRig.transform);
+            
+            // Lưu xuống file nếu đang chạy build
+            #if !UNITY_EDITOR
+            SaveCalibrationToFile();
+            #endif
+            
+            if (showDebugInfo)
+            {
+                Debug.Log($"Headset position saved on quit: {cameraRig.transform.position}");
+            }
         }
     }
 
@@ -35,15 +77,58 @@ public class CalibrateTable : MonoBehaviour
         virtualHeadset.transform.position = cameraRig.transform.position;
         virtualHeadset.transform.rotation = cameraRig.transform.rotation;
     }
+    
     public void AdjustHeadsetRelativeToTableAxis()
     {
-        // Vị trí controller phải trong scene
-        // virtualController.transform.position = new Vector3(8.11170006f, 1.09089994f, -0.333400011f);
-        // virtualController.transform.rotation = Quaternion.Euler(new(44.962574f, 90.1477737f, 166.526947f));
         virtualController.transform.position = virtualControllerTargetAxis.transform.position;
         virtualController.transform.rotation = virtualControllerTargetAxis.transform.rotation;
 
         cameraRig.transform.position = virtualHeadset.transform.position;
         cameraRig.transform.rotation = virtualHeadset.transform.rotation;
     }
+    
+    // Lưu vị trí và góc quay của Headset vào ScriptableObject
+    public void SaveHeadsetPosition()
+    {
+        if (calibrationData != null)
+        {
+            calibrationData.SaveCurrentTransform(cameraRig.transform);
+            
+            #if !UNITY_EDITOR
+            SaveCalibrationToFile();
+            #endif
+
+            if (showDebugInfo)
+            {
+                Debug.Log($"Headset position saved: {cameraRig.transform.position}, Time: {calibrationData.lastCalibrationTime}");
+            }
+        }
+        else
+        {
+            Debug.LogError("Cannot save headset position: calibrationData is null. Assign a HeadsetCalibrationData asset in the Inspector.");
+        }
+    }
+    
+    // Khôi phục vị trí và góc quay của Headset từ ScriptableObject
+    public void LoadHeadsetPosition()
+    {
+        if (calibrationData != null && calibrationData.hasBeenCalibrated)
+        {
+            cameraRig.transform.position = calibrationData.headsetPosition;
+            cameraRig.transform.rotation = calibrationData.headsetRotation;
+
+            if (showDebugInfo)
+            {
+                Debug.Log($"Headset position loaded: {calibrationData.headsetPosition}, Last calibration: {calibrationData.lastCalibrationTime}");
+            }
+        }
+        else
+        {
+            if (showDebugInfo)
+            {
+                Debug.LogWarning("No saved headset calibration data found or calibrationData is null.");
+            }
+        }
+    }
+    
 }
